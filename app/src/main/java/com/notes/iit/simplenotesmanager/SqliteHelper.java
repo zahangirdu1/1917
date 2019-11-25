5240d7963ab253b9c97a9481fa05d5d165ffc197
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2019-11-25 12:39:28 +0600

    added project login and registration local

---
+++ app/src/main/java/com/notes/iit/simplenotesmanager/SqliteHelper.java
@@ -0,0 +1,102 @@
+package com.notes.iit.simplenotesmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by amardeep on 10/26/2017.
+ */
+
+public class SqliteHelper extends SQLiteOpenHelper {
+
+    //DATABASE NAME
+    public static final String DATABASE_NAME = "notes";
+
+    //DATABASE VERSION
+    public static final int DATABASE_VERSION = 1;
+
+    //TABLE NAME
+    public static final String TABLE_USERS = "users";
+
+    //TABLE USERS COLUMNS
+    //ID COLUMN @primaryKey
+    public static final String KEY_ID = "id";
+
+    //COLUMN user name
+    public static final String KEY_USER_NAME = "username";
+
+    //COLUMN email
+    public static final String KEY_EMAIL = "email";
+
+    //COLUMN password
+    public static final String KEY_PASSWORD = "password";
+
+    //SQL for creating users table
+    public static final String SQL_TABLE_USERS = " CREATE TABLE " + TABLE_USERS
+            + " ( "
+            + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + KEY_EMAIL + " TEXT, "
+            + KEY_PASSWORD + " TEXT"
+            + " ) ";
+
+
+    public SqliteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_TABLE_USERS);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_USERS);
+    }
+
+
+    public void addUser(User user) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_EMAIL, user.email);
+
+        values.put(KEY_PASSWORD, user.password);
+
+        long todo_id = db.insert(TABLE_USERS, null, values);
+    }
+
+    public User retreiveUserByEmail(String email){
+        SQLiteDatabase database=this.getReadableDatabase();
+        Cursor cursor=database.query(TABLE_USERS,null,KEY_EMAIL+"=?",new String[]{email},null,null,null);
+        if(cursor!=null&&cursor.getCount()>0){
+            cursor.moveToFirst();
+            String mail=cursor.getString(cursor.getColumnIndex(KEY_EMAIL));
+            String password=cursor.getString(cursor.getColumnIndex(KEY_PASSWORD));
+            User retreivedUser=new User(mail,password);
+            return retreivedUser;
+        }
+        return null;
+    }
+
+
+    public boolean isEmailExists(String email) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS,// Selecting Table
+                null,
+                KEY_EMAIL + "=?",
+                new String[]{email},
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()&& cursor.getCount()>0) {
+            return true;
+        }
+        return false;
+    }
+}
